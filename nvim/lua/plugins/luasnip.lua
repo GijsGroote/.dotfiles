cb4cfccf1a9f155bd7d57d314784b0b73b6dae13
Gijs Groote <gijsgroote@hotmail.com>
2024-05-15 15:28:17 +0200

    remove tab + somethign shortcuts, use tabs a 4 spaces onlyu

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -5,10 +5,19 @@ return {
     config = function()
 
       -- Set mapping functions for luasnip
+      -- vim.cmd[[
+      -- " Use Tab to expand and jump through snippets
+      -- imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+      -- smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+      -- " Use Shift-Tab to jump backwards through snippets
+      -- imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+      -- smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+      -- ]]
       vim.cmd[[
-      " Use Tab to expand and jump through snippets
-      imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-      smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+      " Use Ctrl-P to expand and jump through snippets
+      imap <silent><expr> <C-P> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<C-P>' 
+      smap <silent><expr> <C-P> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<C-P>'
 
       " Use Shift-Tab to jump backwards through snippets
       imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
