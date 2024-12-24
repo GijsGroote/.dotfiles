fdef0005857364b8e2c06e989cebffa50450d7d8
Gijs Groote <gijsgroote@hotmail.com>
2024-12-24 13:32:33 +0100

    switch to latexmk compiler

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,11 +6,7 @@ return {
       "nvim-lua/plenary.nvim", -- optional, for file icons
     },
     keys = {
-      {
-        "<leader>h",
-        [[<Cmd>lua require("harpoon.mark").add_file()<CR><Cmd>lua print("file harpooned!")<CR>]],
-        desc = "harpoon file",
-      },
+      { "<leader>h", [[<Cmd>lua require("harpoon.mark").add_file()<CR><Cmd>lua print("file harpooned!")<CR>]], desc = "harpoon file", },
       { "<leader>H", [[ <Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR> ]], desc = "Show harponed files" },
       { "<leader>1", [[ <Cmd>lua require("harpoon.ui").nav_file(1)<CR> ]], desc = "goto file 1" },
       { "<leader>2", [[ <Cmd>lua require("harpoon.ui").nav_file(2)<CR> ]], desc = "goto file 2" },
--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -2,10 +2,16 @@ return {
   "lervag/vimtex",
   lazy = false,     -- we don't want to lazy load VimTeX
   -- tag = "v2.15", -- uncomment to pin to a specific release
-  -- keys = {},
+  keys = {
+      {"<localleader>lc", ":!latexmk -pvc -pdf %<CR>", desc = "continuous compilation"},
+  },
+
+
   init = function()
     -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_compiler_method = 'latexmk'
     vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_quickfix_mode = 0  -- Disable automatic quickfix list popup
   end
 }
 
