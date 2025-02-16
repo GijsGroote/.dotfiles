53559732a9d59760e9fc42141370cab99ec7ceb2
Gijs Groote <gijsgroote@hotmail.com>
2025-02-16 09:48:47 +0100

    added ads quote

--- nvim/lua/config/arts/personal_quotes.lua
+++ nvim/lua/config/arts/personal_quotes.lua
@@ -1,5 +1,7 @@
 return {
-    {"Embraco, Secop, Danfoss and Tecamseh.                                                         ", 'All billion dollar companies and no one really knows about them', '', '- Gijs Groote'},
-    {"What a joke, for double the price you'll get double the pain.", '', '- Christopher McDougall'},
+    {"Embraco, Secop, Danfoss and Tecamseh.                                                         ", "All billion dollar companies and no one really knows about them", "", "- Gijs Groote"},
+    {"What a joke, for double the price you'll get double the pain.", "", "- Christopher McDougall"},
+
+{"JCDecaux, Global, Clear Channel and Ströer.                                                         ", "Causing distractions, lowering attention spans and trashing the view in public spaces. All in the name of increasing profit margins for their customers. Man I really hate ads.", "", "- Gijs Groote"},
 }
 
