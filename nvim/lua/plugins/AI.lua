a5d517cc752c9b4d393e9faeec614a8c42eec2c9
Gijs Groote <gijsgroote@hotmail.com>
2025-02-27 07:24:29 +0100

    added copilot again

--- nvim/lua/plugins/AI.lua
+++ nvim/lua/plugins/AI.lua
@@ -1,48 +1,50 @@
 
-return {}
+return {
+    {
+        {
+            "zbirenbaum/copilot.lua",
+            cmd = "Copilot",
+            build = ":Copilot auth",
+            event = "InsertEnter",
+            opts = {
+                suggestion = { enabled = true},
+                panel = { enabled = false },
+                filetypes = {
+                    markdown = true,
+                    help = true,
+                },
+            },
+        },
+        {
+            "zbirenbaum/copilot-cmp",
+            config = function ()
+                require("copilot_cmp").setup()
+            end
+        },
 
--- local home = vim.fn.expand("$HOME")
--- return {
---     "jackMort/ChatGPT.nvim",
---     event = "VeryLazy",
---     config = function()
---         require("chatgpt").setup({
---             -- load ChatGPT API Key
---             api_key_cmd = "gpg --decrypt " .. home .. "/secret.txt.gpg"
---         })
---     end,
---     dependencies = {
---         "MunifTanjim/nui.nvim",
---         "nvim-lua/plenary.nvim",
---         "folke/trouble.nvim",
---         "nvim-telescope/telescope.nvim"
---     },
---     keys = {
---         { "<leader>sr", "<cmd>SudaRead<CR>", desc = "sudo read" },
---         { "<leader>sw", "<cmd>SudaWrite<CR>", desc = "sudo write" },
---     },
--- }
+    }
+}
+
+        -- local home = vim.fn.expand("$HOME")
+        -- return {
+            --     "jackMort/ChatGPT.nvim",
+            --     event = "VeryLazy",
+            --     config = function()
+                --         require("chatgpt").setup({
+                    --             -- load ChatGPT API Key
+                    --             api_key_cmd = "gpg --decrypt " .. home .. "/secret.txt.gpg"
+                    --         })
+                    --     end,
+                    --     dependencies = {
+                        --         "MunifTanjim/nui.nvim",
+                        --         "nvim-lua/plenary.nvim",
+                        --         "folke/trouble.nvim",
+                        --         "nvim-telescope/telescope.nvim"
+                        --     },
+                        --     keys = {
+                            --         { "<leader>sr", "<cmd>SudaRead<CR>", desc = "sudo read" },
+                            --         { "<leader>sw", "<cmd>SudaWrite<CR>", desc = "sudo write" },
+                            --     },
+                            -- }
 
 
--- {
---     {
---         "zbirenbaum/copilot.lua",
---         cmd = "Copilot",
---         build = ":Copilot auth",
---         event = "InsertEnter",
---         opts = {
---             suggestion = { enabled = true},
---             panel = { enabled = false },
---             filetypes = {
---                 markdown = true,
---                 help = true,
---             },
---         },
---     },
---     {
---         "zbirenbaum/copilot-cmp",
---         config = function ()
---             require("copilot_cmp").setup()
---         end
---     },
--- }
