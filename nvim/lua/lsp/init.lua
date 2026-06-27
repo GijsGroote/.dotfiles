3b697f8821aa8b84b7e443ab989543d22076f41b
Gijs Groote <gijsgroote@hotmail.com>
2026-06-27 21:57:32 +0200

    add rust LSP back into the game

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -4,7 +4,7 @@ require("mason").setup()
 require('lsp.lsp-setup')
 require('lsp.cmp')
 require('lsp.dap')
--- require('lsp.rust-setup')
+require('lsp.rust-setup')
 require('lsp.python-setup')
 require('lsp.csharp-setup')
 
